39f377f430d8fa227c2c1e3f3680c1bcfbf5e422
Nate <nategiesing@gmail.com>
2017-08-01 10:11:56 -0600

    first iteration done

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,37 @@
 class Pantry
+attr_reader :stock
+
+
+  def initialize
+    @stock = {}
+  end
+
+  def stock_check(restock_item)
+   count = @stock[restock_item]
+   if count == nil
+     count = 0
+   end
+   count
+ end
+
+  def restock(restock_item, quantity)
+    if @stock.has_key?(restock_item)
+      @stock[restock_item] = @stock[restock_item] + quantity
+    elsif restock_item.class != String
+      nil
+    else
+      @stock[restock_item] = quantity
+    end
+  end
+
+  def centi_units(quantity)
+    quantity/100
+  end
+
+  def milli_units(quantity)
+    quantity*1000
+  end
+
+
 
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -4,4 +4,50 @@ require 'minitest/pride'
 
 class PantryTest < Minitest::Test
 
+  def test_it_exist
+    pantry = Pantry.new
+
+    assert_instance_of Pantry, pantry
+  end
+
+  def test_stock_is_a_hash
+    pantry = Pantry.new
+    expected = {}
+
+    assert_instance_of Hash, pantry.stock
+    assert_equal expected, pantry.stock
+  end
+
+  def test_stock_check
+    pantry =  Pantry.new
+    expected = 0
+
+    assert_equal expected, pantry.stock_check("Cheese")
+  end
+
+  def test_it_can_be_restocked_with_ten
+    pantry = Pantry.new
+    pantry.restock("Cheese", 10)
+    result = pantry.stock_check("Cheese")
+
+    assert_equal 10, result
+  end
+
+  def test_it_can_be_restocked_thirty
+    pantry = Pantry.new
+    pantry.restock("Cheese", 10)
+    pantry.restock("Cheese", 20)
+
+    result = pantry.stock_check("Cheese")
+  end
+
+  def test_centi_unit_conversion
+    pantry = Pantry.new
+    result = pantry.centi_units(200)
+
+    assert_equal 2, result
+ end
+
+
+
 end
